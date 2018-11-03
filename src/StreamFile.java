2c86bacff9ab208c122907a55f8ed57e4aa315b7
nbaelen <n.baelen@gmail.com>
2018-11-03 21:00:28 +0100

    Essai Stream avec un fichier

---
+++ src/StreamFile.java
@@ -0,0 +1,27 @@
+import javax.imageio.IIOException;
+import java.util.List;
+import java.util.stream.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class StreamFile {
+    public static void main(String[] args) {
+        String fileName = "sites.txt";
+
+        try(Stream<String> sf = Files.lines(Paths.get(fileName))) {
+            sf.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nLiens en .fr dans la liste donnée");
+        try(Stream<String> sf = Files.lines(Paths.get(fileName))){
+            List<Object> list = sf.filter(x -> x.contains(".fr"))
+                    .collect(Collectors.toList());
+            System.out.println(list);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
