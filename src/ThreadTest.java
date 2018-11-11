cff16a32de95864325c57ad85c1e51f5b92c8629
nbaelen <n.baelen@gmail.com>
2018-11-12 00:38:25 +0100

    Utilisations des Threads

---
+++ src/ThreadTest.java
@@ -0,0 +1,5 @@
+public class ThreadTest {
+    public static void main(String[] args) {
+        System.out.println("Le nom du thread principal est : " + Thread.currentThread().getName());
+    }
+}
