8a9bf70bc1afb0ade5b08c95446ad86ea861341d
nbaelen <n.baelen@gmail.com>
2018-11-12 00:44:21 +0100

    Visualisation de l'ordonnanceur

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,5 +1,22 @@
-public class ThreadTest {
+public class ThreadTest extends Thread {
+    public ThreadTest(String name) {
+        super(name);
+    }
+
+    public void run() {
+        for(int i=0; i<10; i++) {
+            System.out.println(this.getName());
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Le nom du thread principal est : " + Thread.currentThread().getName());
+        ThreadTest t1 = new ThreadTest("A");
+        ThreadTest t2 = new ThreadTest("B");
+        ThreadTest t3 = new ThreadTest("C");
+        ThreadTest t4 = new ThreadTest("D");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 }
